fd7ee15031bfa7fb58f055857c7d8a331ca3bf4f
Lukas <lukasdevarga@gmail.com>
2021-03-01 23:56:09 +0100

    emotion/react and some footer styles added

--- components/footer/index.js
+++ components/footer/index.js
@@ -3,14 +3,24 @@ import styles from '../../styles/Home.module.css'
 
 const Footer = () => (
   <footer className={ styles.footer }>
-    <a
+    <div>
+      <p>Copyright &copy;2021 All rights reserved</p>
+    </div>
+    <div className={ styles.socialFooter }>
+      <p>My social side</p>
+      <a href="#" className="fa fa-linkedin"></a>
+      <a href="#" className="fa fa-twitter"></a>
+      <a href="#" className="fa fa-instagram"></a>
+    </div>
+
+    {/* <a
       href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
       target="_blank"
       rel="noopener noreferrer"
     >
       Created by a Lucky guy with the power of{ ' ' }
       <img src="/vercel.svg" alt="Vercel Logo" className={ styles.logo } />
-    </a>
+    </a> */}
   </footer>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,7 @@ export default function Home() {
       <Head>
         <title>Iridium Blog</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </Head>
 
       <main className={ styles.main }>
